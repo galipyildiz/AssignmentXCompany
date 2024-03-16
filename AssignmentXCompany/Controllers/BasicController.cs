9ee7e9d13037b451a4859a272c3c05a44f131ad4
galipyildiz <galipyildiz9606@outlook.com>
2024-03-16 11:48:12 +0300

    add: Basic C# Skills

---
+++ AssignmentXCompany/Controllers/BasicController.cs
@@ -0,0 +1,32 @@
+﻿using AssignmentXCompany.Services.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentXCompany.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BasicController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+
+        [HttpGet]
+        public IActionResult AvarageofInputs([FromQuery] int[] numbers)
+        {
+            try
+            {
+                var result = BasicService.Avarage(numbers);
+                return Ok($"Result: {result:N2}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ AssignmentXCompany/Services/Concrete/BasicService.cs
@@ -0,0 +1,22 @@
+﻿namespace AssignmentXCompany.Services.Concrete
+{
+    public static class BasicService
+    {
+        public static double Avarage(params int[] array)
+        {
+            if (array is null)
+                throw new ArgumentNullException(nameof(array));
+
+            if (array.Length == 0)
+                throw new Exception("array length cannot be zero");
+
+            var sum = 0;
+            foreach (var i in array)
+            {
+                sum += i;
+            }
+            var avarage = (double)sum / array.Length;
+            return avarage;
+        }
+    }
+}
